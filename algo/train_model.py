e5f7c6059d65b0c58b29940330bf3adb32cf09cc
Mun Kar Kin <karkin.mun@C02ZJ1QTLVDQ.local>
2020-07-28 19:22:07 +0800

    changed path

--- algo/train_model.py
+++ algo/train_model.py
@@ -2,13 +2,8 @@
 # @Author  :  KarKin
 # @FileName: worker.py
 
-import os
-
-print(os.getcwd())
-print(os.listdir())
-
 from sklearn import datasets
-from algo.linear_regression import LinearModel
+from linear_regression import LinearModel
 
 model = LinearModel(datasets.load_boston())
 model.train()
