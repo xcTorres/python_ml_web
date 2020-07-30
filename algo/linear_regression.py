bf7680f5f57277993810078145ffdcfe7bac75d7
Mun Kar Kin <karkin.mun@C02ZJ1QTLVDQ.local>
2020-07-30 10:02:07 +0800

    kompose and minikube steps

--- algo/linear_regression.py
+++ algo/linear_regression.py
@@ -32,7 +32,7 @@ class LinearModel(object):
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=5)
         self.model.fit(x_train, y_train)
 
-        with open('algo/linear_regression.pkl', 'wb') as f:
+        with open('./algo/linear_regression.pkl', 'wb') as f:
             pickle.dump(self.model, f)    
 
         # model evaluation for training set
